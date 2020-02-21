bc7c27502a95fe79c1b6f8f7b6afcf6c679bd404
Anjjar Brahim <anjar.bra@gmail.com>
2020-02-21 22:41:08 +0100

    New modification

--- app.c
+++ app.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
+#include<graphics.h>
+#include<aphics.h>
 
 void main(){
 
 	printf("Hello world!");
+	
+	for(int i = 0; i > 10; i++){
+		 for(int j = 0; j > 10; j++){
+			 printf("*");
+		 }
+	}
+	
 	
 	return 0;
 }
